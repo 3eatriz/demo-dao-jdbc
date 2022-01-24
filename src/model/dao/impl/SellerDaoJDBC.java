75e81f91902536e2f5703ddcf9c7ea0e8f5f35c3
Beatriz <beatrizfreitasti@gmail.com>
2022-01-24 12:38:26 -0300

    Implementacao insert

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -24,13 +24,51 @@ public class SellerDaoJDBC  implements SellerDao{
 	}
 	
 	@Override
-	public void insert(Seller d) {
-		// TODO Auto-generated method stub
+	public void insert(Seller obj) {
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)",
+					java.sql.Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+			
+				DB.closeResultSet(rs);
+				
+			}
+			else {
+					throw new DbException("Erro inesperado! Nenhuma linhda foi afetada!");
+			}
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
-	public void update(Seller d) {
+	public void update(Seller s) {
 		// TODO Auto-generated method stub
 		
 	}
