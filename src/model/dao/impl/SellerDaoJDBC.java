b89efbdd8cc8b5d509ee582ba87c5ebbf63f96d4
Beatriz <beatrizfreitasti@gmail.com>
2022-01-24 12:48:53 -0300

    Implementacao update

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -48,15 +48,12 @@ public class SellerDaoJDBC  implements SellerDao{
 				if(rs.next()) {
 					int id = rs.getInt(1);
 					obj.setId(id);
-				}
-			
-				DB.closeResultSet(rs);
-				
+				}	
+				DB.closeResultSet(rs);	
 			}
 			else {
 					throw new DbException("Erro inesperado! Nenhuma linhda foi afetada!");
 			}
-			
 		}
 		catch(SQLException e) {
 			throw new DbException(e.getMessage());
@@ -64,13 +61,33 @@ public class SellerDaoJDBC  implements SellerDao{
 		finally {
 			DB.closeStatement(st);
 		}
-		
 	}
 
 	@Override
-	public void update(Seller s) {
-		// TODO Auto-generated method stub
+	public void update(Seller obj) {
+PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
